10d855d543f206fb0e345ec7ef2e614d873b19ce
fimorr <fimorrr@yandex.ru>
2019-05-17 20:53:11 +0500

    initial commit

---
+++ WebContent/Component.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
+	"use strict";
+
+	return UIComponent.extend("example.ui.test.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+	});
+
+});

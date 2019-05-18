8da01dae3e22bb7534755864d4593c2df52a717d
fimorr <fimorrr@yandex.ru>
2019-05-18 10:44:31 +0500

    Добавление i18n значений, реализация логики работы документов и образования

--- WebContent/controller/Form.controller.js
+++ WebContent/controller/Form.controller.js
@@ -5,7 +5,42 @@ sap.ui.define([
 
 	return Controller.extend("example.ui.test.controller.Form", {
 		
+		handleAddEducationItem(oEvent) {
+			let oView = this.getView();
+			let oButton = oEvent.getSource();
+			let oForm = oButton.getParent().getParent();
+			
+			let aItem = sap.ui.xmlfragment(oView.getId(), "example.ui.test.fragment.educationItem", this);
+			
+			aItem.forEach((oItem) => {
+				oView.addDependent(oItem);
+				oForm.insertFormElement(oItem);
+			});
+			//oView.addDependent(oItem);
+			
+			//debugger;
+			
+			console.log(aItem);
+			
+		},	
 		
-		
+		passportFormatter: function(sValue) {
+			if (sValue === "passport") {
+				return true;
+			}
+			return false;
+		},
+		driverlicenseFormatter: function(sValue) {
+			if (sValue === "driverlicense") {
+				return true;
+			}
+			return false;
+		},	
+		militaryidFormatter: function(sValue) {
+			if (sValue === "militaryid") {
+				return true;
+			}
+			return false;
+		}
 	});
 });

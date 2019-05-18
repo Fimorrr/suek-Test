b731e3452cd7609d2e4b884b6c5fc5927e782093
fimorr <fimorrr@yandex.ru>
2019-05-18 18:21:11 +0500

    Добавлена валидация для всех контролов

--- WebContent/controller/Form.controller.js
+++ WebContent/controller/Form.controller.js
@@ -1,28 +1,181 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/SimpleType",
+	'sap/ui/model/ValidateException'
+], function (Controller, SimpleType, ValidateException) {
 	"use strict";
 
 	return Controller.extend("example.ui.test.controller.Form", {
 		
-		handleAddEducationItem(oEvent) {
+		onInit: function() {
+			
+			var oView = this.getView();
+			var aInputs = this.getAllItems(oView.byId("SimpleFormToolbar"), "sap.m.Input");
+			
+			aInputs.forEach((oInput) => {
+				sap.ui.getCore().getMessageManager().registerObject(oInput, true);
+				oInput.attachChange(this.handleInputChange.bind(this));
+			});
+		},
+		
+		handleBirthdayChange: function(oEvent) {
+			let oModel = this.getOwnerComponent().getModel("values");
+			let oDatePicker = oEvent.getSource();
+			
+			let oDateValue = oDatePicker.getDateValue();
+			let oDateNow = new Date();
+			
+			let fDiff = oDateNow - oDateValue;
+			
+			if (fDiff < 0) {
+				oModel.setProperty("/age", "");
+			} 
+			else {
+				let iYearsBetween = Math.abs(new Date(fDiff).getUTCFullYear() - 1970);
+				oModel.setProperty("/age", iYearsBetween);
+			}
+			
+			this._validateDatePicker(oDatePicker);
+		},
+		
+		handleDateChange: function(oEvent) {
+			let oDatePicker = oEvent.getSource();
+			
+			this._validateDatePicker(oDatePicker);
+		},
+		
+		handleInputChange: function(oEvent) {
+			let oInput = oEvent.getSource();
+			
+			this._validateInput(oInput);
+		},
+		
+		handleAddEducationItem: function(oEvent) {
 			let oView = this.getView();
-			let oButton = oEvent.getSource();
-			let oForm = oButton.getParent().getParent();
+			let oForm = oView.byId("SimpleFormToolbar");
 			
 			let aItem = sap.ui.xmlfragment(oView.getId(), "example.ui.test.fragment.educationItem", this);
 			
 			aItem.forEach((oItem) => {
 				oView.addDependent(oItem);
-				oForm.insertFormElement(oItem);
+				oForm.addContent(oItem);
 			});
 			//oView.addDependent(oItem);
 			
 			//debugger;
 			
-			console.log(aItem);
+			//console.log(aItem);
 			
 		},	
+
+		handleContinue : function () {
+			let that = this;
+			let oView = this.getView();
+			let aInput = this.getAllItems(oView.byId("SimpleFormToolbar"), "sap.m.Input");
+			let aDatePicker = this.getAllItems(oView.byId("SimpleFormToolbar"), "sap.m.DatePicker");
+			let bValidationError = false;
+
+			aInput.forEach((oInput) => {
+				bValidationError = this._validateInput(oInput) || bValidationError;
+			});
+			
+			aDatePicker.forEach((oDatePicker) => {
+				bValidationError = this._validateDatePicker(oDatePicker) || bValidationError;
+			});
+			
+			console.log(bValidationError);
+		},
+		
+		getAllItems : function (oView, sName) {
+			let aContent = oView.getContent();
+			let aItem = [];
+			
+			aContent.forEach((oItem) => {
+				if (oItem.getVisible() && oItem.getMetadata().getName() === sName) {
+					aItem = [...aItem, oItem];
+				}
+			});
+			
+			return aItem;
+		}, 
+		
+		_validateInput: function(oInput) {
+			let oBinding = oInput.getBinding("value");
+			let sValueState = "None";
+			let bValidationError = false;
+
+			if (oBinding == null) {
+				if (oInput.getValue() == "") {
+					sValueState = "Error";
+					bValidationError = true;
+				}
+			}
+			else {
+				try {
+					oBinding.getType().validateValue(oInput.getValue());
+				} catch (oException) {
+					sValueState = "Error";
+					bValidationError = true;
+				}
+			}
+
+			oInput.setValueState(sValueState);
+
+			return bValidationError;
+		},
+		
+		_validateDatePicker: function(oDatePicker) {
+			let oDateValue = oDatePicker.getDateValue();
+			let sValueState = "None";
+			let bValidationError = false;
+			
+			if (oDateValue == null) {
+				sValueState = "Error";
+				bValidationError = true;
+			}
+			else {
+				let oDateNow = new Date();
+				
+				let fDiff = oDateNow - oDateValue;
+				
+				if (fDiff < 0) {
+					sValueState = "Error";
+					bValidationError = true;
+				} 
+			}
+			
+			oDatePicker.setValueState(sValueState);
+			return bValidationError;
+		},
+
+		nameType : SimpleType.extend("Text", {
+			formatValue: function (oValue) {
+				return oValue;
+			},
+			parseValue: function (oValue) {
+				return oValue;
+			},
+			validateValue: function (oValue) {
+				var regex = /^[a-zA-Zа-яА-Я ]+$/;
+				if (!oValue.match(regex)) {
+					throw new ValidateException("Неверные данные");
+				}
+			}
+		}),
+		
+		ageType : SimpleType.extend("Text", {
+			formatValue: function (oValue) {
+				return oValue;
+			},
+			parseValue: function (oValue) {
+				return oValue;
+			},
+			validateValue: function (oValue) {
+				if (oValue < 0 || oValue > 150) {
+					throw new ValidateException("Неверные данные");
+				}
+			}
+		}),
 		
 		passportFormatter: function(sValue) {
 			if (sValue === "passport") {
